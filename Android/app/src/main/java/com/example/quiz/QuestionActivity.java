6bc8bc9dae20c3e4386bf20b571e507826374383
seros76 <69792587+seros76@users.noreply.github.com>
2020-11-11 12:00:44 +0100

    question class result activity

--- Android/app/src/main/java/com/example/quiz/QuestionActivity.java
+++ Android/app/src/main/java/com/example/quiz/QuestionActivity.java
@@ -7,22 +7,28 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import nl.dionsegijn.konfetti.KonfettiView;
 import nl.dionsegijn.konfetti.models.Shape;
 import nl.dionsegijn.konfetti.models.Size;
 
-public class Question extends AppCompatActivity {
-    private static final String LOG_TAG = Question.class.getSimpleName();
+public class QuestionActivity extends AppCompatActivity {
+    public static final String QUESTION_NUMBER = "com.example.quiz.extra.MESSAGE";
+    public static final String SCORE = "com.example.quiz.extra.MESSAGE";
+    private static final String LOG_TAG = QuestionActivity.class.getSimpleName();
 
     private int correctAnswer;
     private int currentQuestion;
-    private int score;
     private int questionNumber;
+    private int score;
+
+    private List<question> questions;
 
     private ProgressBar pgTimer;
     private TextView tvProgress;
@@ -40,10 +46,13 @@ public class Question extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
+        //set initial values
+        currentQuestion = 0;
+        score = 0;
+
         pgTimer = findViewById(R.id.quiz_progressBar);
         tvProgress = findViewById(R.id.textView_questionProgress);
         konfettiView = findViewById(R.id.viewKonfetti);
-
         tvQuestion = findViewById(R.id.textView_questionText);
         buttonAnswerA = findViewById(R.id.textView_answerA);
         buttonAnswerB = findViewById(R.id.textView_answerB);
@@ -55,6 +64,9 @@ public class Question extends AppCompatActivity {
         Bundle extras = intent.getExtras();
         questionNumber = extras.getInt(MainActivity.QUESTION_NUMBER, 5);
         tvProgress.setText("Question " + currentQuestion + " out of " + questionNumber);
+
+        //read right amount of questions from DB
+        questions = getQuestions();
 
         //Button Listeners
         buttonAnswerA.setOnClickListener(new View.OnClickListener() {
@@ -90,23 +102,38 @@ public class Question extends AppCompatActivity {
         });
 
         //create first question
-        createQuestion();
+        createQuestion(currentQuestion);
 
         Log.d(LOG_TAG, "-----");
         Log.d(LOG_TAG, "on create");
     }
 
+    //TODO: read random questions from FirebaseDB
+    private List<question> getQuestions() {
+        Log.d(LOG_TAG, "new question list created!");
 
-    private void createQuestion() {
-        //TODO: get Question from FirebaseDB and set data accordingly
-        tvQuestion.setText("Question");
+        List<question> questions = new ArrayList<>();
 
-        buttonAnswerA.setText("Option A");
-        buttonAnswerB.setText("Option B");
-        buttonAnswerC.setText("Option C");
-        buttonAnswerD.setText("Option D");
+        String question = "What is the capital of France?";
+        String[] answers = {"Germany", "Switzerland", "France", "USA"};
+        for (int i = 0; i < questionNumber; i++) {
+            questions.add(new question(question, 2, answers));
+        }
 
-        correctAnswer = 1;
+        return questions;
+    }
+
+    //create new question from list and set UI accordingly
+    private void createQuestion(int i) {
+        question q = questions.get(i);
+        tvQuestion.setText(q.getQuestion());
+
+        buttonAnswerA.setText(q.getAnswer(0));
+        buttonAnswerB.setText(q.getAnswer(1));
+        buttonAnswerC.setText(q.getAnswer(2));
+        buttonAnswerD.setText(q.getAnswer(3));
+
+        correctAnswer = q.getCorrectAnswer();
 
         Log.d(LOG_TAG, "new question created!");
     }
@@ -147,13 +174,14 @@ public class Question extends AppCompatActivity {
         Log.d(LOG_TAG, "onDestroy");
     }
 
+    //check for correct answer and change UI
     private void submit(int answer) {
         Log.d(LOG_TAG, "checking if answer correct");
+        currentQuestion = currentQuestion + 1;
 
         //correct answer, fireworks!
         if (answer == correctAnswer) {
             score = score + 1;
-            currentQuestion = currentQuestion + 1;
 
             konfettiView.build()
                     .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
@@ -204,13 +232,28 @@ public class Question extends AppCompatActivity {
                 }
             }
         }
+
+        //more questions to answer? if not, display quiz results
+        if (currentQuestion < questionNumber) {
+            createQuestion(currentQuestion);
+            tvProgress.setText("Question " + currentQuestion + " out of " + questionNumber);
+        } else {
+            displayResult();
+        }
+
+
     }
 
     // when game is over, display result
     private void displayResult() {
         Log.d(LOG_TAG, "display QuizResultActivity!");
+        Bundle extras = new Bundle();
+        extras.putInt(QUESTION_NUMBER, questionNumber);
+        extras.putInt(SCORE, score);
         Intent intent = new Intent(this, QuizResultActivity.class);
+        intent.putExtras(extras);
         startActivity(intent);
+
     }
 
 }
