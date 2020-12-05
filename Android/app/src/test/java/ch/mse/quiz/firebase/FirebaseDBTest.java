a81c9903f3c1a06217c1bd0af0e7b24d70113477
SteinerPascal <st_pascal@outlook.com>
2020-12-05 15:21:45 +0100

    add Firebasetest

--- Android/app/src/test/java/ch/mse/quiz/firebase/FirebaseDBTest.java
+++ Android/app/src/test/java/ch/mse/quiz/firebase/FirebaseDBTest.java
@@ -1,89 +1,91 @@
 package ch.mse.quiz.firebase;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.ktx.Firebase;
 
 import junit.framework.TestCase;
 
-import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.robolectric.RobolectricTestRunner;
 
-import androidx.test.filters.RequiresDevice;
-import ch.mse.quiz.models.question;
+import java.sql.DriverManager;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
-import static android.content.ContentValues.TAG;
+import ch.mse.quiz.MainActivity;
 
-@RunWith(RobolectricTestRunner.class)
+
+@RunWith(PowerMockRunner.class)
 public class FirebaseDBTest extends TestCase {
 
-  DatabaseReference dbRef;
-  FirebaseDatabase database = FirebaseDatabase.getInstance();
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-  @Override
-  @Before
-  public void setUp() throws Exception {
-    super.setUp();
-    // 10.0.2.2 is the special IP address to connect to the 'localhost' of
-    // the host computer from an Android emulator.
-    database.useEmulator("192.168.8.106", 9000);
-  }
+    @Mock
+    public MainActivity mainActivity;
 
-  @Test
-  public void testGetTopics() {
-    dbRef = database.getReference("topics");
+    @Mock
+    FirebaseApp firebaseApp;
+    @Mock
+    FirebaseDatabase firebaseDatabase;
+    @Mock
+    DatabaseReference dbRef;
 
-    ArrayList<String> topics = new ArrayList<String>();
 
-    dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
-      @Override
-      public void onDataChange(DataSnapshot dataSnapshot) {
-        //Getting the string value of that node
-        Iterable<DataSnapshot> children = dataSnapshot.getChildren();
-        children.forEach(i -> {
-          topics.add(i.getKey());
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        PowerMockito.mockStatic(FirebaseApp.class);
+        BDDMockito.given(FirebaseApp.getInstance()).willReturn(firebaseApp);
+
+        PowerMockito.mockStatic(FirebaseDatabase.class);
+        BDDMockito.given(FirebaseDatabase.getInstance()).willReturn(firebaseDatabase);
+        Mockito.when(firebaseDatabase.getReference(Mockito.anyString())).thenReturn(dbRef);
+
+
+
+
+        //FirebaseApp.initializeApp(mainActivity.getBaseContext());
+        //database = FirebaseDatabase.getInstance();
+        // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+        // the host computer from an Android emulator.
+        //mainActivity.database.useEmulator("192.168.8.106", 9000);
+    }
+
+    @Test
+    public void testGetQuestions() throws ExecutionException, InterruptedException {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(new Runnable() {
+            @Override
+            public void run() {
+                // Simulate a long-running Job
+                try {
+                    mainActivity.getTopics();
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException(e);
+                }
+                System.out.println("I'll run in a separate thread than the main thread.");
+            }
         });
-        String[] topicSelection = topics.toArray(new String[topics.size()]);
-      }
-      @Override
-      public void onCancelled(DatabaseError databaseError) {
-        //TODO maybe change this to special test?
-        assertTrue("Databseerror happened",false);
-      }
-    });
-    assertTrue("List contains 3 topics",topics.size() == 3);
-  }
 
-  @Test
-  public void testGetQuestions() {
-    dbRef = database.getReference("topics/history/questions");
-    // must be array because of anonymous inner class
-    final int[] questionnr = new int[1];
-    dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
-      @Override
-      public void onDataChange(DataSnapshot dataSnapshot) {
-        //check if amount of question is h
-        questionnr[0] = (int) dataSnapshot.getChildrenCount();
-      }
+// Block and wait for the future to complete
+        future.get();
 
-      @Override
-      public void onCancelled(DatabaseError databaseError) {
-        //TODO maybe change this to special test?
-        assertTrue("Databseerror happened",false);
-      }
-    });
-    assertTrue("The result should be 10 questions", questionnr[0] == 10 );
-  }
-
-
-
-
+    }
 
 
 }
